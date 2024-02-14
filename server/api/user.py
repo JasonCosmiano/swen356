91085fb9fda5ae452c99c8b303f421362ade2006
JasonCosmiano <cosmianoj21@gmail.com>
2024-02-14 00:45:10 -0500

    user class

---
+++ server/api/user.py
@@ -0,0 +1,62 @@
+from flask import request
+
+from flask_restful import Resource, reqparse
+from .db_utils import *
+
+class User(Resource):
+
+
+    def get(self):
+       """
+       Get a user's info
+       Currently waiting on DB for sql command
+       """
+
+       sql_command = """
+        """
+       result = exec_get_all_as_dict(sql_command)
+       return result
+    
+    def put(self):
+       """Update with body params"""
+
+       # body params
+       parser = reqparse.RequestParser()
+       parser.add_argument('currentBook', type=str)
+       parser.add_argument('bookList', type=str)
+       parser.add_argument('friendList', type=str)
+       parser.add_argument('readingStats', type=str)
+       
+       args = parser.parse_args()
+       currentBook = args['currentBook']
+       bookList = args['bookList']
+       friendList = args['friendList']
+       readingStats = args['readingStats']
+       
+       # update statement, pending on DB
+       sql_command = """
+        """
+       result = exec_commit(sql_command, (currentBook, bookList, friendList, readingStats))
+       return result
+    
+    def post(self):
+       """POST with body params"""
+
+       # body params
+       parser = reqparse.RequestParser()
+       parser.add_argument('currentBook', type=str)
+       parser.add_argument('bookList', type=str)
+       parser.add_argument('friendList', type=str)
+       parser.add_argument('readingStats', type=str)
+       
+       args = parser.parse_args()
+       currentBook = args['currentBook']
+       bookList = args['bookList']
+       friendList = args['friendList']
+       readingStats = args['readingStats']
+       
+       # INSERT INTO statement, pending on DB
+       sql_command = """
+        """
+       result = exec_commit(sql_command, (currentBook, bookList, friendList, readingStats))
+       return result
---
+++ server/server.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_restful import Resource, Api
+from flask_cors import CORS
+
+from api.db_utils import *
+
+from api.user import User
+
+app = Flask(__name__) #create Flask instance
+CORS(app) #Enable CORS on Flask server to work with Nodejs pages
+api = Api(app) #api router
+
+api.add_resource(User, '/categories')
+
+if __name__ == '__main__':
+    
+    print("Loading db")
+    # sql file pending
+    # exec_sql_file('people.sql')
+    print("Starting flask")
+    app.run(debug=True), #starts Flask
