769d4b670346f7f3cfe5c0ed70e31d2293abd432
JasonCosmiano <cosmianoj21@gmail.com>
2024-02-23 01:55:50 -0500

    tests not working rn

--- server/api/user.py
+++ server/api/user.py
@@ -15,166 +15,6 @@ class User(Resource):
        result = exec_get_all_as_dict(sql_command)
        return result
     
-    def put(self):
-       """Update with body params"""
-
-       # body params
-       parser = reqparse.RequestParser()
-       parser.add_argument('username', type=str)
-       parser.add_argument('password', type=str)
-       parser.add_argument('email', type=str)
-       parser.add_argument('currentBook', type=str)
-       parser.add_argument('user_id', type=str)
-       
-       args = parser.parse_args()
-       username = args['username']
-       password = args['password']
-       email = args['email']
-       currentBook = args['currentBook'] 
-       user_id = args['user_id'] 
-
-       # init messages
-       message = "PUT command falied"
-       a_dict = {'message':message} 
-
-       # only update username
-       if password == None and email == None and currentBook == None:
-          sql_command = """
-                UPDATE Users 
-                SET username=%s
-                WHERE user_id=%s
-            """
-          result = exec_commit(sql_command, (username, user_id))
-
-      # only update password
-       elif username == None and email == None and currentBook == None:
-          sql_command = """
-                UPDATE Users 
-                SET password=%s
-                WHERE user_id=%s
-            """
-          result = exec_commit(sql_command, (password, user_id))
-
-      # only email 
-       elif username == None and password == None and currentBook == None:
-          sql_command = """
-                UPDATE Users 
-                SET email=%s
-                WHERE user_id=%s
-            """
-          result = exec_commit(sql_command, (password, user_id))
-
-      # only currentBook password
-       elif username == None and password == None and email == None:
-          sql_command = """
-                UPDATE Users 
-                SET currentBook=%s
-                WHERE user_id=%s
-            """
-          result = exec_commit(sql_command, (password, user_id))
-
-      # only username, password, email
-       elif currentBook == None:
-          sql_command = """
-                UPDATE Users 
-                SET username=%s, password=%s, email=%s
-                WHERE user_id=%s
-            """
-          result = exec_commit(sql_command, (username, password, email, user_id))
-
-      # only currentBook, password, email
-       elif username == None:
-          sql_command = """
-                UPDATE Users 
-                SET currentBook=%s, password=%s, email=%s
-                WHERE user_id=%s
-            """
-          result = exec_commit(sql_command, (currentBook, password, email, user_id))
-
-
-      # only currentBook, password, username
-       elif email == None:
-          sql_command = """
-                UPDATE Users 
-                SET currentBook=%s, password=%s, username=%s
-                WHERE user_id=%s
-            """
-          result = exec_commit(sql_command, (currentBook, password, username, user_id))
-
-      # only currentBook, email, username
-       elif password == None:
-          sql_command = """
-                UPDATE Users 
-                SET currentBook=%s, email=%s, username=%s
-                WHERE user_id=%s
-            """
-          result = exec_commit(sql_command, (currentBook, email, username, user_id))
-
-      # only email, username
-       elif password == None and currentBook == None:
-          sql_command = """
-                UPDATE Users 
-                SET username=%s, email=%s
-                WHERE user_id=%s
-            """
-          result = exec_commit(sql_command, (username, email, user_id))
-
-      # only password, username
-       elif email == None and currentBook == None:
-          sql_command = """
-                UPDATE Users 
-                SET username=%s, password=%s
-                WHERE user_id=%s
-            """
-          result = exec_commit(sql_command, (username, password, user_id))
-
-      # only currentBook, username
-       elif email == None and password == None:
-          sql_command = """
-                UPDATE Users 
-                SET username=%s, currentBook=%s
-                WHERE user_id=%s
-            """
-          result = exec_commit(sql_command, (username, currentBook, user_id))
-      
-      # only password, currentBook
-       elif username == None and email == None:
-          sql_command = """
-                UPDATE Users 
-                SET password=%s, currentBook=%s
-                WHERE user_id=%s
-            """
-          result = exec_commit(sql_command, (password, currentBook, user_id))
-   
-      # only password, email
-       elif username == None and currentBook == None:
-          sql_command = """
-                UPDATE Users 
-                SET password=%s, email=%s
-                WHERE user_id=%s
-            """
-          result = exec_commit(sql_command, (password, email, user_id))
-
-      # only currentBook, email
-       elif username == None and password == None:
-          sql_command = """
-                UPDATE Users 
-                SET currentBook=%s, email=%s
-                WHERE user_id=%s
-            """
-          result = exec_commit(sql_command, (currentBook, email, user_id))
-
-      # update everything
-       else:
-          sql_command = """
-                UPDATE Users 
-                SET currentBook=%s, email=%s, password=%s, username=%s
-                WHERE user_id=%s
-            """
-          result = exec_commit(sql_command, (currentBook, email, password, username, user_id))
-
-       return result
-    
     def post(self):
        """POST with body params"""
 
@@ -196,8 +36,12 @@ class User(Resource):
             INSERT INTO Users(username, password, email, currentBook)
             VALUES(%s, %s, %s, %s, %s, %s, %s)
         """
-       result = exec_commit(sql_command, (username, password, email, currentBook))
-       return result
+       result = exec_insert_update_delete(sql_command, (username, password, email, currentBook))
+       
+       if result == 0:
+          return "FAILED TO CREATE NEW USER"
+       
+       return "POST SUCCESS"
     
 class SingleUser(Resource):
 
@@ -215,8 +59,173 @@ class SingleUser(Resource):
 
       result = exec_get_all_as_dict(sql_command, {'_id':id}) # returns number of records
 
-      if result == 0:
+      if result == []:
          return "USER DOES NOT EXIST"
       
       return result
+   
+   def put(self, id):
+       """Update with body params"""
+
+       # body params
+       parser = reqparse.RequestParser()
+       parser.add_argument('username', type=str)
+       parser.add_argument('password', type=str)
+       parser.add_argument('email', type=str)
+       parser.add_argument('currentBook', type=str)
+    #    parser.add_argument('user_id', type=str)
+       
+       args = parser.parse_args()
+       username = args['username']
+       password = args['password']
+       email = args['email']
+       currentBook = args['currentBook'] 
+    #    user_id = args['user_id'] 
+
+    #    # init messages
+    #    message = "PUT command falied"
+    #    a_dict = {'message':message} 
+
+       # only update username
+       if password == None and email == None and currentBook == None:
+          sql_command = """
+                UPDATE Users 
+                SET username=%s
+                WHERE user_id=%s
+            """
+          result = exec_insert_update_delete(sql_command, (username, str(id)))
+
+      # only update password
+       elif username == None and email == None and currentBook == None:
+          sql_command = """
+                UPDATE Users 
+                SET password=%s
+                WHERE user_id=%s
+            """
+          result = exec_insert_update_delete(sql_command, (password, str(id)))
+
+      # only email 
+       elif username == None and password == None and currentBook == None:
+          sql_command = """
+                UPDATE Users 
+                SET email=%s
+                WHERE user_id=%s
+            """
+          result = exec_insert_update_delete(sql_command, (password, str(id)))
+
+      # only currentBook password
+       elif username == None and password == None and email == None:
+          sql_command = """
+                UPDATE Users 
+                SET currentBook=%s
+                WHERE user_id=%s
+            """
+          result = exec_insert_update_delete(sql_command, (password, str(id)))
+
+      # only username, password, email
+       elif currentBook == None:
+          sql_command = """
+                UPDATE Users 
+                SET username=%s, password=%s, email=%s
+                WHERE user_id=%s
+            """
+          result = exec_insert_update_delete(sql_command, (username, password, email, str(id)))
+
+      # only currentBook, password, email
+       elif username == None:
+          sql_command = """
+                UPDATE Users 
+                SET currentBook=%s, password=%s, email=%s
+                WHERE user_id=%s
+            """
+          result = exec_insert_update_delete(sql_command, (currentBook, password, email, str(id)))
+
+
+      # only currentBook, password, username
+       elif email == None:
+          sql_command = """
+                UPDATE Users 
+                SET currentBook=%s, password=%s, username=%s
+                WHERE user_id=%s
+            """
+          result = exec_insert_update_delete(sql_command, (currentBook, password, username, str(id)))
+
+      # only currentBook, email, username
+       elif password == None:
+          sql_command = """
+                UPDATE Users 
+                SET currentBook=%s, email=%s, username=%s
+                WHERE user_id=%s
+            """
+          result = exec_insert_update_delete(sql_command, (currentBook, email, username, str(id)))
+
+      # only email, username
+       elif password == None and currentBook == None:
+          sql_command = """
+                UPDATE Users 
+                SET username=%s, email=%s
+                WHERE user_id=%s
+            """
+          result = exec_insert_update_delete(sql_command, (username, email, str(id)))
+
+      # only password, username
+       elif email == None and currentBook == None:
+          sql_command = """
+                UPDATE Users 
+                SET username=%s, password=%s
+                WHERE user_id=%s
+            """
+          result = exec_insert_update_delete(sql_command, (username, password, str(id)))
+
+      # only currentBook, username
+       elif email == None and password == None:
+          sql_command = """
+                UPDATE Users 
+                SET username=%s, currentBook=%s
+                WHERE user_id=%s
+            """
+          result = exec_insert_update_delete(sql_command, (username, currentBook, str(id)))
+      
+      # only password, currentBook
+       elif username == None and email == None:
+          sql_command = """
+                UPDATE Users 
+                SET password=%s, currentBook=%s
+                WHERE user_id=%s
+            """
+          result = exec_insert_update_delete(sql_command, (password, currentBook, str(id)))
+   
+      # only password, email
+       elif username == None and currentBook == None:
+          sql_command = """
+                UPDATE Users 
+                SET password=%s, email=%s
+                WHERE user_id=%s
+            """
+          result = exec_insert_update_delete(sql_command, (password, email, str(id)))
+
+      # only currentBook, email
+       elif username == None and password == None:
+          sql_command = """
+                UPDATE Users 
+                SET currentBook=%s, email=%s
+                WHERE user_id=%s
+            """
+          result = exec_insert_update_delete(sql_command, (currentBook, email, str(id)))
+
+      # update everything
+       else:
+          sql_command = """
+                UPDATE Users 
+                SET currentBook=%s, email=%s, password=%s, username=%s
+                WHERE user_id=%s
+            """
+          result = exec_insert_update_delete(sql_command, (currentBook, email, password, username, str(id)))
+          
+
+          
+       if (result == 0):
+            return "PUT command failed"
+       
+       return "PUT command success"
     
--- server/tests/api/test_user.py
+++ server/tests/api/test_user.py
@@ -18,4 +18,52 @@ class TestUser(unittest.TestCase):
         expected = 1
         actual = get_rest_call(self, 'http://localhost:5000/user/1') # invoke
         self.assertEqual(expected, len(actual)) # analyze
-        print("TEST_GET_ALL_USERS PASS")
+        print("TEST_GET_USER PASS")
+
+    def test_get_nonexistent_user(self):
+        """Unit test to get a user that does not exist"""
+        expected = "USER DOES NOT EXIST"
+        actual = get_rest_call(self, 'http://localhost:5000/user/15') # invoke
+        self.assertEqual(expected, actual) # analyze
+        print("TEST_GET_NONEXISTENT_USERS PASS")
+
+    def test_post_new_user(self):
+        """Add user"""
+        print("\nTEST_POST_NEW_USER")
+
+        # make new user
+        _username = "Pedri"
+        _password = "password123"
+        _email = "moneymunson@rit.edu"
+        _currentBook = "2"
+
+        # body
+        data = dict(username=_username, password=_password, email=_email, currentBook=_currentBook)
+        jdata =json.dumps(data)
+        print("TEST_POST_NEW_USER MAKING USER: ", data)
+
+        result = post_rest_call(self, "http://localhost:5000/users", jdata)
+        self.assertEqual(result, 1)  
+        # print("TEST_POST_NEW_USER MAKING USER: ", result)
+        print("TEST_POST_NEW_USER USER SUCCESSFULLY ADDED")
+
+
+    def test_update_user(self):
+        """Unit test to update a specific user"""
+        
+        print("\nTEST_PUT_USER")
+    
+        # edit user
+        _username = "jumanji"
+        # _password = "password123"
+        # _email = "moneymunson@rit.edu"
+        # _currentBook = "2"
+
+         # body
+        data = dict(username=_username)
+        jdata =json.dumps(data)
+        print("TEST_UPDATE_USER UPDATE USER WITH ID 1, WITH: " + str(data))
+
+        result = put_rest_call(self, "http://localhost:5000/user/1", jdata, {}, 200)
+        self.assertEqual(result, "PUT command success")
+        print("TEST_PUT_USER RESULT: ", str(result))
