31e04cfe1adbb00e27b67e1c7970212dc93aacb5
JasonCosmiano <cosmianoj21@gmail.com>
2024-03-15 16:40:31 -0400

    friend activity

--- server/api/friend.py
+++ server/api/friend.py
@@ -48,3 +48,19 @@ class Friend(Resource):
           return "FAILED TO FOLLOW FRIEND"
 
        return "FOLLOW FRIEND SUCCESS"    
+    
+class FriendActivity(Resource):
+   
+   def get(self, id):
+      """
+      Get friend's activity
+      """
+      sql_command = """
+         SELECT books.id AS "book_id", books.title, books.genre, books.author 
+         FROM books
+         INNER JOIN booklist on booklist.bookid = books.id
+         WHERE booklist.userid = %(_id)s;
+      """
+
+      result = exec_get_all_as_dict(sql_command, {'_id':id})
+      return result
