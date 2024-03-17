b7835bd24f247a8490c6b8abe084cac2f7ea96ba
Robert Mastropieri <rmastropieri14@gmail.com>
2024-03-16 22:54:00 -0400

    book info

---
+++ server/api/bookinfo.py
@@ -0,0 +1,67 @@
+from flask import request
+
+from flask_restful import Resource, reqparse
+from server.api import booklist
+from .db_utils import *
+
+class BookInfo(Resource):
+    def get(self, id):
+        """
+        Get details of a book
+        """
+        sql_command = """
+            SELECT * FROM Books WHERE id = %s;
+        """
+        book = exec_get_one(sql_command, (id))
+        
+        if book:
+            return book
+        else:
+            return "Book not found"
+        
+    def post(self, id):
+        """
+        Add a review for the book
+        """
+        parser = reqparse.RequestParser()
+        parser.add_argument('review_content', type=str, required=True)
+        args = parser.parse_args()
+        review_content = args['review_content']
+
+        if not review_content:
+            return "Review content missing"
+
+        sql_command = """
+            INSERT INTO Reviews(book_id, content)
+            VALUES (%s, %s);
+        """
+        result = exec_insert_update_delete(sql_command, (id, review_content))
+
+        if result:
+            return "Review added successfully"
+        else:
+            return "Failed to add review"
+        
+class AddToReadingList(Resource):
+    def post(self, id):
+        """
+        Add a book to the user's reading list
+        """
+        parser = reqparse.RequestParser()
+        parser.add_argument('book_id', type=int, required=True)
+        args = parser.parse_args()
+        book_id = args['book_id']
+
+        if not book_id:
+            return "Book ID missing", 400
+
+        sql_command = """
+            INSERT INTO BookList(user_id, book_id)
+            VALUES (%s, %s);
+        """
+        result = exec_insert_update_delete(sql_command, (id, book_id))
+
+        if result:
+            return "Book added to reading list successfully"
+        else:
+            return "Failed to add book to reading list"
